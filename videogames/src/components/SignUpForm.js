4fae6c8b73072cb25cf0848cd06760c97f188fe7
Michael Bowman <micbowman25@gmail.com>
2020-05-28 16:48:14 -0500

    added values

--- videogames/src/components/SignUpForm.js
+++ videogames/src/components/SignUpForm.js
@@ -3,19 +3,27 @@ import { useFormik } from 'formik'
 
 function SignUpForm() {
 
-    const formik = useFormik({})
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    })
+
+    console.log('Form values', formik.values)
 
     return(
         <div>
             <form>
                 <label htmlFor='email'>E-mail</label>
-                <input type='text' id='email' name='email' />
+                <input type='text' id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
 
                 <label htmlFor='password'>Password</label>
-                <input type='text' id='password' name='password' />
+                <input type='text' id='password' name='password' onChange={formik.handleChange} value={formik.values.password} />
 
                 <label htmlFor='confirmPassword'>Confirm Password</label>
-                <input type='text' id='confirmPassword' name='confirmPassword' />
+                <input type='text' id='confirmPassword' name='confirmPassword' onChange={formik.handleChange} value={formik.values.confirmPassword} />
 
                 <button>Submit</button>
             </form>

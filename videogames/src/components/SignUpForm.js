4473aecd3fbb672c21ae4e87a035eddc8acef2c7
Michael Bowman <micbowman25@gmail.com>
2020-06-13 15:37:04 -0500

    added required field errors and validation

--- videogames/src/components/SignUpForm.js
+++ videogames/src/components/SignUpForm.js
@@ -1,31 +1,91 @@
 import React from 'react';
 import { useFormik } from 'formik'
 
-function SignUpForm() {
+const initialValues = {
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
 
+const onSubmit = values => {
+    console.log('Form data', values)
+}
+
+const validate = values => {
+    let errors = {}
+
+    if(!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email format'
+    }
+
+    if(!values.password) {
+        errors.password = 'Required'
+    }
+
+    if(!values.confirmPassword) {
+        errors.confirmPassword = 'Required'
+    }
+
+    return errors
+}
+
+function SignUpForm() {
     const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-            confirmPassword: ''
-        }
+        initialValues,
+        onSubmit,
+        validate
     })
 
     console.log('Form values', formik.values)
 
     return(
         <div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
+                <div className='form-control'>
                 <label htmlFor='email'>E-mail</label>
-                <input type='text' id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
+                <input 
+                  type='text' 
+                  id='email' 
+                  name='email' 
+                  onChange={formik.handleChange} 
+                  value={formik.values.email} 
+                />
+                
+                {formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
+                
+                </div>
 
+                <div className='form-control'>
                 <label htmlFor='password'>Password</label>
-                <input type='text' id='password' name='password' onChange={formik.handleChange} value={formik.values.password} />
+                <input 
+                  type='text' 
+                  id='password' 
+                  name='password' 
+                  onChange={formik.handleChange} 
+                  value={formik.values.password} 
+                />
+                
+                {formik.errors.password ? <div className='error'>{formik.errors.password}</div> : null}
+                
+                </div>
 
+                <div className='form-control'>
                 <label htmlFor='confirmPassword'>Confirm Password</label>
-                <input type='text' id='confirmPassword' name='confirmPassword' onChange={formik.handleChange} value={formik.values.confirmPassword} />
+                <input 
+                  type='text' 
+                  id='confirmPassword' 
+                  name='confirmPassword' 
+                  onChange={formik.handleChange} 
+                  value={formik.values.confirmPassword} 
+                />
+                
+                {formik.errors.confirmPassword ? <div className='error'>{formik.errors.confirmPassword}</div> : null}
+                
+                </div>
 
-                <button>Submit</button>
+                <button type='submit'>Submit</button>
             </form>
         </div>
     )
